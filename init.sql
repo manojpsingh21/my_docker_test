40ff6ea2d86f2494fdd385a4dda6f8cc7f3c79a6
manoj <manoj@gmail.com>
2024-08-24 20:35:19 +0530

    Create user added

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+CREATE USER 'presql'@'localhost' IDENTIFIED BY 'Rupee@123';
 GRANT ALL PRIVILEGES ON *.* TO 'presql'@'%d' WITH GRANT OPTION;
 ALTER USER 'presql'@'%' IDENTIFIED WITH mysql_native_password BY 'Rupee@123';
 FLUSH PRIVILEGES;
+SET GLOBAL max_connections = 6000;
