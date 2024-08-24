3d92c3ef16a7f98472056c3a2aaf466e791995e2
manoj <manoj@gmail.com>
2024-08-24 22:59:31 +0530

    Grant sorted and cmad for connection in compose

--- init.sql
+++ init.sql
@@ -1,5 +1,3 @@
--- CREATE USER 'presql'@'localhost' IDENTIFIED BY 'Rupee@123';
--- GRANT ALL PRIVILEGES ON *.* TO 'presql'@'%d' WITH GRANT OPTION;
--- ALTER USER 'presql'@'%' IDENTIFIED WITH mysql_native_password BY 'Rupee@123';
--- FLUSH PRIVILEGES;
--- SET GLOBAL max_connections = 6000;
+GRANT ALL PRIVILEGES ON *.* TO 'presql'@'%' WITH GRANT OPTION;
+ALTER USER 'presql'@'%' IDENTIFIED WITH mysql_native_password BY 'Rupee@123';
+FLUSH PRIVILEGES;
