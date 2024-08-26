f2052b7542f724277ebb7e2bce7a09ffe3e5257f
manoj <manoj@gmail.com>
2024-08-26 09:11:45 +0530

    Shellscript added

---
+++ AutoStartSystem.sh
@@ -0,0 +1,29 @@
+append=`date "+%Y%m%d_%H%M"`
+append1=`date`
+echo "stop OMS"
+pwd
+cd $HOME/Application/Exec/MonitoringTools
+pwd
+./StopApplication.sh
+echo "sucessfully stop OMS"
+
+sleep 10
+
+cd $HOME/Application/Exec/ShellScripts
+./StartApplication.sh
+echo "sucessfully start OMS"
+
+sleep 10
+if test "$1 " = "1 "; then
+	./ConnectExch.sh NSEEQ
+	echo "Started NseCMExchAdap"
+	./ConnectExch.sh NSEDR
+	echo "Started NseFOExchAdap"
+	#./ConnectExch.sh NSECUR
+	#echo "Start CNseConAdap"
+	./ConnectExch.sh BSEEQ
+	echo "Started BseCMExchAdap"
+	./ConnectExch.sh BSECUR
+	echo "Started BseCDExchAdap"
+fi;
+
