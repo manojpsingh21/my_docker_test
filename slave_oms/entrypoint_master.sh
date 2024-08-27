0708ffb39f710de10e72a5206b16d3ba73f16a3f
manoj <manoj@gmail.com>
2024-08-27 23:37:47 +0530

    test

--- slave_oms/entrypoint_master.sh
+++ slave_oms/entrypoint_master.sh
@@ -1,5 +1,7 @@
 dos2unix ~/rupeeseed.env ~/.bashrc ~/Application/Exec/ShellScripts/AutoStartSystem.sh 
+source ~/.bashrc
 cd ~/Application/Exec/MonitoringTools/
 find . -type f -exec dos2unix {} \;
 cd ~/Application/Exec/ShellScripts/
 find . -type f -exec dos2unix {} \;
+~/Application/Exec/ShellScripts/AutoStartSystem.sh & 
